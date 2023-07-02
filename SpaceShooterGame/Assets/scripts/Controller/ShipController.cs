ede36254b632344ccac303cba458435044f6391a
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-07-02 06:59:23 -0700

    impl shipController
    Implementation of the ship movement system

---
+++ SpaceShooterGame/Assets/scripts/Controller/ShipController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    #region PublicVaribles 
+    [Range(5,100)]
+    public float Speed = 5f;
+    #endregion
+
+    private void Start()
+    {
+        
+    }
+
+   private void Update()
+    {
+        // حرکت پلیر
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        transform.position += Speed * Time.deltaTime * new Vector3(x,y,0);
+    }
+}
